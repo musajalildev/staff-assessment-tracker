8ca4507cfff48d4457ad4e5d0bf475124ce54120
Benjamin Whalley <benjaminjwhalley@outlook.com>
2025-11-18 09:46:13 +0000

    fix@persistence: added missing annotations

--- server/src/main/java/com/assessment/tracker/server/persistence/assessment/AssessmentRepo.java
+++ server/src/main/java/com/assessment/tracker/server/persistence/assessment/AssessmentRepo.java
@@ -1,5 +1,7 @@
 package com.assessment.tracker.server.persistence.assessment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssessmentRepo extends JpaRepository<Assessment, Integer> {}
--- server/src/main/java/com/assessment/tracker/server/persistence/moduleUserRole/ModuleUserRoleRepo.java
+++ server/src/main/java/com/assessment/tracker/server/persistence/moduleUserRole/ModuleUserRoleRepo.java
@@ -1,4 +1,6 @@
 package com.assessment.tracker.server.persistence.moduleUserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModuleUserRoleRepo extends JpaRepository<ModuleUserRole,Integer> {}
