fdd1c63878979f44bef9cbdfff5c527cc6a05bfe
aca24iu <iuteh-obuseh1@sheffield.ac.uk>
2025-11-29 17:30:19 +0000

    Removed `AssessmentStage` entity, repository, and service.

--- server/src/main/java/com/assessment/tracker/server/persistence/assessmentStage/AssessmentStageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assessment.tracker.server.persistence.assessmentStage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssessmentStageRepository extends JpaRepository<AssessmentStage, String> {
-}
