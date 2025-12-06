670bdfd3f6581f23ab01799f4bb503b1c91329a9
Jose Gomes <jgomes2@sheffield.ac.uk>
2025-12-06 13:04:52 +0000

    Quick fix to allow access to h2-console

--- server/src/main/java/com/assessment/tracker/server/configuration/SecurityConfiguration.java
+++ server/src/main/java/com/assessment/tracker/server/configuration/SecurityConfiguration.java
@@ -17,7 +17,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,7 +34,8 @@ import com.assessment.tracker.server.persistence.services.JpaUserDetailsService;
 @EnableMethodSecurity
 public class SecurityConfiguration {
     /*
-     * This is to be rewritten default implementation is to permit any request we will have
+     * This is to be rewritten default implementation is to permit any request we
+     * will have
      * Roles etc. therefore we need to use spring security roles
      */
 
@@ -51,8 +51,7 @@ public class SecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 // allow H2 console (optional)
-                .headers(headers -> headers.frameOptions
-                        (frame -> frame.sameOrigin()))
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
 
                 // disable CSRF because you are using JWT (stateless)
                 .csrf(AbstractHttpConfigurer::disable)
@@ -60,24 +59,20 @@ public class SecurityConfiguration {
                 // authorization rules
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login", "/auth/signup").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         // sgnup is only like this for testing should be only permitted for TST
                         // -> TODO: account creation only done by TST
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
 
                 // JWT validation (your RSA public key via Nimbus)
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                )
+                        .jwt(Customizer.withDefaults()))
 
                 // do not use session (JWT = stateless)
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -97,7 +92,8 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
