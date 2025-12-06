7e0eb13388aade6999b6c62a090d3c41c584ab05
Benjamin Whalley <benjaminjwhalley@outlook.com>
2025-12-06 15:10:59 +0000

    doc: added open api descriptions to Assessment and Module controllers

--- server/src/main/java/com/assessment/tracker/server/api/controller/ModuleController.java
+++ server/src/main/java/com/assessment/tracker/server/api/controller/ModuleController.java
@@ -1,35 +1,52 @@
 package com.assessment.tracker.server.api.controller;
 
-import com.assessment.tracker.server.api.controller.*;
-import com.assessment.tracker.server.api.controllerImpl.*;
-import com.assessment.tracker.server.api.DTO.*;
-
-import com.assessment.tracker.server.persistence.entities.*;
-import com.assessment.tracker.server.persistence.repos.*;
-import com.assessment.tracker.server.persistence.services.*;
-
-import com.assessment.tracker.server.utils.mappers.*;
-import com.assessment.tracker.server.utils.enums.*;
+import com.assessment.tracker.server.api.dto.*;
 
 import java.io.InputStream;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 public interface ModuleController {
 
-        @GetMapping(path = "/api/v1/module", produces = "application/json", consumes = "application/json")
+        @GetMapping(
+                path = "/api/v1/module",
+                produces = "application/json",
+                consumes = "application/json")
+        @Operation(
+                summary = "Gets module based on module code",
+                description = "Returns respective module object")
+        @Tag(name = "Module", description = "Module-Related-Operations")
         ModuleDTO getModule(@RequestParam(required = false) String moduleCode);
 
         @PutMapping(path = "/api/v1/module", consumes = "application/json")
+        @Operation(
+                summary = "Update module based on updated schema",
+                description = "Returns respective module object")
+        @Tag(name = "Module", description = "Module-Related-Operations")
         ResponseEntity<ModuleDTO> updateModule(@RequestBody ModuleDTO module);
 
         @GetMapping(path = "/api/v1/module", produces = "application/json")
+        @Operation(
+                summary = "Update module based on updated schema",
+                description = "Returns respective module object")
+        @Tag(name = "Module", description = "Module-Related-Operations")
         List<ModuleDTO> getModules();
 
         @PostMapping(path = "/api/v1/module", consumes = "application/json")
+        @Operation(
+                summary = "Create module based on schema",
+                description = "Returns response entity if the creation is successful")
+        @Tag(name = "Module", description = "Module-Related-Operations")
         ResponseEntity<ModuleDTO> createModule(ModuleDTO module);
 
         @PostMapping(path = "/api/v1/module/csv", consumes = "text/csv")
+        @Operation(
+                summary = "Create module based on CSV",
+                description = "Returns whether the operation is successful")
+        @Tag(name = "Module", description = "Module-Related-Operations")
         ResponseEntity<ModuleDTO> createModuleCSV(@RequestBody InputStream csv);
 }
